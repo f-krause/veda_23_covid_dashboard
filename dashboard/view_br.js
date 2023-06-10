d6941ad641855202d2dc899f643bf5ea20e6fe02
Felix Krause <felix_krause1999@yahoo.de>
2023-06-10 16:32:34 +0200

    second mwe of view_br

--- dashboard/view_br.js
+++ dashboard/view_br.js
@@ -2,153 +2,153 @@
 
 // Load data
 d3.csv("../data/vacc.csv").then(data_raw => {
-    // Save list of countries for later use
-    const countries = data_raw.map(d => d.country); 
-    
-    // Date converter
-    var timeParser = d3.timeParse("%Y-%m-%d");
 
-    // Transform data to new format
-    let data = [];
-    data_raw.forEach(e => {
-        let currCountry = e.country;
-        delete e.country;
+// Save list of countries for later use
+const countries = data_raw.map(d => d.country); 
 
-        Object.entries(e).forEach(
-            ([key, value]) => data.push({
-                country: currCountry,
-                date: timeParser(key),      // Change datatype to number
-                vacc:  +value
-            })
-        );
-    });
-    console.log(data)
+// Date converter
+var timeParser = d3.timeParse("%Y-%m-%d");
 
-    // Group data by country
-    const data_grouped = d3.group(data, d => d.country);
-    
+// Transform data to new format
+let data = [];
+data_raw.forEach(e => {
+    let currCountry = e.country;
+    delete e.country;
 
-    // Set up svg
-    const w = 1000;
-    const h = 400;
-    const x_padding = 100;
-    const y_padding = 50;
-
-    const svg = d3.select("body")
-                .append("svg")
-                .attr("width",  w)
-                .attr("height", h)
-                .attr("id", "lineChart");
-
-
-    // Add y axis
-    const yScale = d3.scaleLinear()
-                        .domain([0, d3.max(data.map(d => d.vacc))])
-                        .range([h - y_padding, y_padding]);
-
-    const yAxis = d3.axisLeft(yScale);
-
-    svg.append("g")
-        .attr("transform", "translate(" + (x_padding) + ",0)")
-        .call(yAxis);
-
-
-    // Add x axis
-    const xScale = d3.scaleTime()
-                     .domain(d3.extent(data, function(d){return d.date}))
-                     .range([x_padding, w - x_padding*1.5]);
-
-    const xAxis = d3.axisBottom(xScale);
-
-    // xAxis.ticks().tickFormat((d, i) => d.toString())
-
-    svg.append("g")
-        .attr("transform", "translate(0," + (h - y_padding) + ")")
-        .call(xAxis);
-
-
-    // Plot lines
-    let line = d3.line()
-        .x(function(d) {
-            return xScale(d.date);
+    Object.entries(e).forEach(
+        ([key, value]) => data.push({
+            country: currCountry,
+            date: timeParser(key),      // Change datatype to number
+            vacc:  +value
         })
-        .y(function(d) {
-            return yScale(d.vacc);
-        });
+    );
+});
+console.log(data)
 
-    var colorScale = d3.scaleOrdinal()
-        .domain(countries)
-        .range(d3.schemeTableau10)
-
-    svg.append("g")
-        .selectAll("path")
-        .data(data_grouped)
-        .join("path")
-            .attr("stroke", d => colorScale(d[0]))
-            .attr("fill", "none")
-            .attr("stroke-width", 2)
-            .attr("d", d => line(d[1]))
-            .on("click", clickOn)
-            .on("mouseover", hoverOn)
-            .on("mouseout", hoverOff)
+// Group data by country
+const data_grouped = d3.group(data, d => d.country);
 
 
-    // Hover effect functions
-    function hoverOn(event, d) {
-        d3.select(this)
-            .transition().duration(80)
-            .attr("stroke-width", 4.5)
-            .attr("stroke", "red")
-
-        svg.append("text")
-            .text(d[0])
-            .attr("x", xScale(d[1].slice(-1)[0].date) + 12)
-            .attr("y", yScale(d[1].slice(-1)[0].vacc) + 6)
-            .attr("class", "textBox")
-        }
-
-    function hoverOff(event, d) {
-        d3.select(this)
-            .transition()
-            .attr("stroke-width", 2)
-            .attr("stroke", colorScale(d[0]))
-
-        svg.selectAll(".textBox").remove()
-        }
+// Get proportions of svg
+const svg = d3.select("#right_col2")
+console.log(svg._groups[0][0]["clientHeight"])
+const w = svg._groups[0][0]["clientWidth"];
+const h = svg._groups[0][0]["clientHeight"];
+const x_padding = 100;
+const y_padding = 0;
 
 
-    // Click effect functions
-    function clickOn(event, d) { 
-        svg.append("path")
-            .attr("stroke", "red")
-            .attr("fill", "none")
-            .attr("stroke-width", 4.5)
-            .attr("d", l => line(d[1]))
+console.log(svg)
+// Add y axis
+const yScale = d3.scaleLinear()
+                    .domain([0, d3.max(data.map(d => d.vacc))*1.05]) // increase max for aesthetics
+                    .range([h - y_padding, y_padding]);
 
-        svg.append("text")
-            .text(d[0])
-            .attr("x", xScale(d[1].slice(-1)[0].date) + 12)
-            .attr("y", yScale(d[1].slice(-1)[0].vacc) + 6)
+const yAxis = d3.axisLeft(yScale);
+
+svg.append("g")
+    .attr("transform", "translate(" + (x_padding) + ",0)")
+    .call(yAxis);
+
+
+// Add x axis
+const xScale = d3.scaleTime()
+                    .domain(d3.extent(data, function(d){return d.date}))
+                    .range([x_padding, w - x_padding*1.5]);
+
+const xAxis = d3.axisBottom(xScale)
+                .tickFormat(d3.timeFormat('%d-%b %y'))
+                //.ticks(d3.timeMonth.every(1))
+
+// xAxis.ticks().tickFormat((d, i) => d.toString())
+
+svg.append("g")
+    .attr("transform", "translate(0," + (h - y_padding) + ")")
+    .call(xAxis);
+
+
+// Plot lines
+let line = d3.line()
+    .x(function(d) {
+        return xScale(d.date);
+    })
+    .y(function(d) {
+        return yScale(d.vacc);
+    });
+
+var colorScale = d3.scaleOrdinal()
+    .domain(countries)
+    .range(d3.schemeTableau10)
+
+svg.append("g")
+    .selectAll("path")
+    .data(data_grouped)
+    .join("path")
+        .attr("stroke", d => colorScale(d[0]))
+        .attr("fill", "none")
+        .attr("stroke-width", 2)
+        .attr("d", d => line(d[1]))
+        .on("click", clickOn)
+        .on("mouseover", hoverOn)
+        .on("mouseout", hoverOff)
+
+
+// Hover effect functions
+function hoverOn(event, d) {
+    d3.select(this)
+        .transition().duration(80)
+        .attr("stroke-width", 4.5)
+        .attr("stroke", "red")
+
+    svg.append("text")
+        .text(d[0])
+        .attr("x", xScale(d[1].slice(-1)[0].date) + 12)
+        .attr("y", yScale(d[1].slice(-1)[0].vacc) + 6)
+        .attr("class", "textBox")
+    }
+
+function hoverOff(event, d) {
+    d3.select(this)
+        .transition()
+        .attr("stroke-width", 2)
+        .attr("stroke", colorScale(d[0]))
+
+    svg.selectAll(".textBox").remove()
     }
 
 
-    // Add x axis label
+// Click effect functions
+function clickOn(event, d) { 
+    svg.append("path")
+        .attr("stroke", "red")
+        .attr("fill", "none")
+        .attr("stroke-width", 4.5)
+        .attr("d", l => line(d[1]))
+
     svg.append("text")
-    .attr("class", "x label")
-    .attr("text-anchor", "end")
-    .attr("x", w/2)
-    .attr("y", h - y_padding + 45)
-    .text("Dates");
+        .text(d[0])
+        .attr("x", xScale(d[1].slice(-1)[0].date) + 12)
+        .attr("y", yScale(d[1].slice(-1)[0].vacc) + 6)
+}
 
 
-    // Add y axis label
-    svg.append("text")
-    .attr("class", "y label")
-    .attr("text-anchor", "middle")
-    .attr("x", -h/2)
-    .attr("y", x_padding - 70)
-    .attr("transform", "rotate(-90)")
-    .text("Nominal GDP (Millions of current dollars)"); // TODO
+// Add x axis label
+// svg.append("text")
+// .attr("class", "x label")
+// .attr("text-anchor", "end")
+// .attr("x", w/2)
+// .attr("y", h - y_padding + 45)
+// .text("Dates");
+
+
+// Add y axis label
+svg.append("text")
+.attr("class", "y label")
+.attr("text-anchor", "middle")
+.attr("x", -h/2)
+.attr("y", x_padding - 60)
+.attr("transform", "rotate(-90)")
+.text("New vaccinations (per 1 mil.)"); // TODO
 
 });
 
